53947a184b68049ea624a8f689a548c8f7641f15
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-06-10 21:59:49 -0700

    moved html files to view folder, added JS on makeres and viewres, moved node_modules to gitignore

--- restaurant.js
+++ restaurant.js
@@ -22,48 +22,44 @@ let waitList = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./view/index.html"));
+});
+
+app.get("/makeres", function(req, res) {
+  res.sendFile(path.join(__dirname, "./view/makeres.html"));
 });
 
 app.get("/viewres", function(req, res) {
-  res.sendFile(path.join(__dirname, "viewres.html"));
+  res.sendFile(path.join(__dirname, "./view/viewres.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitList);
 });
+
+
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
+app.post("/api/tables", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  let newTable = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newTable);
 
-  console.log(newCharacter);
+  if (tables.length < 5){
+    tables.push(newTable);
+  } else {
+    waitList.push(newTable);
+  }
 
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
+  console.log(tables);
+  console.log(waitList);
+  res.json(newTable);
 });
 
 // Starts the server to begin listening
