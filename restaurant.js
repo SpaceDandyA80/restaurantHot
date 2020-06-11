24abab690e5c143ca44d2a220ccafba6fe66f82e
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-06-10 19:56:52 -0700

    adding html with minimal server js

---
+++ restaurant.js
@@ -0,0 +1,73 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 2500;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Restaurant Tables (DATA)
+// =============================================================
+let tables = [];
+let waitList = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/viewres", function(req, res) {
+  res.sendFile(path.join(__dirname, "viewres.html"));
+});
+
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+  return res.json(characters);
+});
+
+// Displays a single character, or returns false
+app.get("/api/characters/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
